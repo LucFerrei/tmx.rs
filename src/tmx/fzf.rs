6aa401a3530fe12ba18fbae8245d710d2904edb7
lucas <lucascamposfrr@gmail.com>
2026-02-17 18:09:43 -0300

    fix: double dirs in fzf

--- src/tmx/fzf.rs
+++ src/tmx/fzf.rs
@@ -2,6 +2,7 @@ use std::process;
 use inquire::ui::{Color, RenderConfig, Styled};
 use inquire::{InquireError, Select};
 use walkdir::WalkDir;
+use super::get_last_two_components;
 
 use super::super::mode::tmux;
 
@@ -30,19 +31,42 @@ pub fn get_inactive_dirs() -> Vec<String> {
         .collect()
 }
 
+fn sanitize_dir(active_sessions: Vec<String>, inactive_dirs: Vec<String>) -> Vec<String> {
+    let mut sanitized: Vec<String> = Vec::new();
+    let mut inactive: Vec<String> = Vec::new();
+
+    for element in inactive_dirs {
+        let last_two = get_last_two_components(&element);
+        let mut real_element = String::from("[TMUX] ");
+
+        real_element.push_str(&last_two);
+        // let cmp = real_element.replace('.', "_");
+
+        if active_sessions.contains(&real_element) {continue;}
+        else { inactive.push(element); }
+    }
+    sanitized.extend(active_sessions);
+    sanitized.extend(inactive);
+
+    sanitized
+}
+
 pub fn fzf(mut active_sessions: Option<Vec<String>>, inactive: Vec<String>) -> Result<String, InquireError> {
 
     let render_config = RenderConfig::default()
             .with_prompt_prefix(Styled::new("🔍").with_fg(Color::LightBlue))
             .with_highlighted_option_prefix(Styled::new("👉").with_fg(Color::LightYellow));
 
-    let mut dirs: Vec<String> = Vec::new();
+    let dirs: Vec<String>;
 
     if let Some(dir) = active_sessions {
-        dirs.extend(dir);
+        dirs = sanitize_dir(dir, inactive);
+        // dirs.extend(dir);
+    }else {
+        dirs = sanitize_dir(Vec::new(), inactive);
     }
 
-    dirs.extend(inactive);
+    // dirs.extend(inactive);
     let answer = Select::new("Select a directory:", dirs)
         .with_render_config(render_config)
         .prompt()?;
